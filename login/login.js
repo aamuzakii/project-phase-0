e541a8f6f7261817fbbd58186f240ce70ddaccd6
Ahmad Faisal <achmad_fsl@yahoo.com>
2021-04-23 08:09:00 +0700

    fitur login

---
+++ login/login.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    this.sessionStorage.setItem("username", "coba")
+    this.sessionStorage.setItem("password", "123")
+}
+
+let input = document.getElementsByTagName('input')
+let login = document.getElementById('submitButton')
+
+login.onclick = () => {
+    if (input[0].value === this.sessionStorage.getItem("username") && input[1].value === this.sessionStorage.getItem("password")) {
+        window.open("https://www.youtube.com/", "_self")
+        alert('Login Berhasil')
+    } else{
+        alert('Silahkan masukkan username : "coba" dan password "123"')
+    }
+}
